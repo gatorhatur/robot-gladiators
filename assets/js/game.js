ea11f9b835eeca16d6c89accecacc9fbf263c979
Brandon Davis <brandon.davis@conehealth.com>
2022-06-11 21:16:56 -0400

    start and end game function

--- assets/js/game.js
+++ assets/js/game.js
@@ -72,18 +72,55 @@ var fight = function(enemyName) {
 
 };
 
+
 //fight();
+var startGame = function() {
 
-for(var i =  0; i < enemyNames.length; i++){
+    //reset player stats
+    playerHealth = 100;
+    playerAttack = 10;
+    playerMoney = 10;
 
-    if(playerHealth > 0){    
-        window.alert("Welcome to Robot Gladiators! Round " + (i + 1)) ;
+    for(var i =  0; i < enemyNames.length; i++){
 
-        var pickedEnemyName = enemyNames[i];    
+        if(playerHealth > 0){    
+            window.alert("Welcome to Robot Gladiators! Round " + (i + 1)) ;
 
-        var enemyHealth = 50;
+            var pickedEnemyName = enemyNames[i];    
 
-        fight(pickedEnemyName);
+            var enemyHealth = 50;
+
+            fight(pickedEnemyName);
+        }
+        else{
+            window.alert("You lost your robot in battle! Game Over");
+            break;
+        }
+
     }
 
-}
+    endGame();
+
+};
+
+var endGame = function() {
+    //if player is still alive, player wins!
+    if(playerHealth > 0){
+        window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
+    }
+    else{
+        window.alert("You've lost your robot in battle.")
+    }
+
+    var playAgainConfirm = window.confirm("Would you like to play again?");
+
+    if(playAgainConfirm){
+        //restart the game
+        startGame();
+    }
+    else{
+        window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+    }
+};
+
+startGame();
